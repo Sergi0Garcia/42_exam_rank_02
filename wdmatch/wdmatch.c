e9f9c5ef797f62d4e12b6f60528c786573881964
Sergio Garcia <segarcia@student.42heilbronn.de>
2022-10-24 12:12:03 +0200

    wdmatch

--- wdmatch/wdmatch.c
+++ wdmatch/wdmatch.c
@@ -6,24 +6,36 @@
 /*   By: segarcia <segarcia@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 11:21:40 by segarcia          #+#    #+#             */
-/*   Updated: 2022/10/24 11:26:43 by segarcia         ###   ########.fr       */
+/*   Updated: 2022/10/24 12:11:20 by segarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char **argv)
 {
 	int i;
+	int j;
 
 	i = 0;
+	j = 0;
 	if (argc == 3)
 	{
-		while ()
+		while (argv[2][j])
 		{
-			/* code */
+			if (argv[2][j] == argv[1][i])
+				i++;
+			j++;
 		}
-
+		if (!argv[1][i])
+		{
+			i = 0;
+			while (argv[1][i])
+			{
+				write(1, &argv[1][i], 1);
+				i++;
+			}
+		}
 	}
 	write(1, "\n", 1);
 }
